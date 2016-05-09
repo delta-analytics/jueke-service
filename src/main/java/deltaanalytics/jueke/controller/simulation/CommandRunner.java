500be333a3bd5151d6ce2a99beebe6a24e9d88f0
spektro001 <spektro001@t-online.de>
2016-05-09 14:57:22 +0200

    Scheduler bei Simulation ausgeschaltet

---
+++ src/main/java/deltaanalytics/jueke/controller/simulation/CommandRunner.java
@@ -0,0 +1,9 @@
+package deltaanalytics.jueke.controller.simulation;
+
+import org.springframework.context.annotation.Profile;
+import org.springframework.stereotype.Service;
+
+@Profile("simulation")
+@Service
+public class CommandRunner {
+}
--- src/main/java/deltaanalytics/jueke/hardware/CommandRunner.java
+++ src/main/java/deltaanalytics/jueke/hardware/CommandRunner.java
@@ -10,10 +10,11 @@ import deltaanalytics.jueke.hardware.serial.JuekeSerialConnectionFactory;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Profile;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Service;
 
-
+@Profile("production")
 @Service
 public class CommandRunner {
     private static final Logger LOGGER = LoggerFactory.getLogger(CommandRunner.class);
